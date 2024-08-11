821e89e2d6af6e36f85bc2c5497f9ff1cecf0c1d
owen-wang-student <owang420@gmail.com>
2024-08-11 10:11:58 -0400

    implemented basic chaining

---
+++ ice_breaker.py
@@ -0,0 +1,35 @@
+import os 
+from dotenv import load_dotenv
+
+from langchain.prompts.prompt import PromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+
+if __name__ == "__main__":
+  load_dotenv()
+
+  # variables inside of curly brackets represent parameters 
+  summary_template = """
+    given the Linkedin information {information} about a person I want you to create:
+    1. A short summary
+    2. two interesting facts about them
+  """
+
+  # information to be used as a variable 
+  information="""
+    Insert some information 
+  """
+
+  # create a prompt template object 
+  summary_prompt_template = PromptTemplate(template=summary_template, input_variables=["information"])
+
+  # creates a wrapper around gpt model (temperature = creativeness)
+  llm = ChatOpenAI(api_key=os.environ["OPENAI_API_KEY"], temperature=0, model_name="gpt-3.5-turbo")
+
+  # uses langchain expression language to send prompt to llm to output parser
+  chain = summary_prompt_template | llm | StrOutputParser()
+
+  # run chain 
+  res = chain.invoke(input={"information": information})
+  
+  print(res)
